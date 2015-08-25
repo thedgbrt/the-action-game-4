75b6315660513876a2a6c46ea1ff61c9cf2cda1f
Mihai Banulescu <mihai.banulescu@gmail.com>
2015-08-25 10:48:36 +0300

    resolved spork start-up issues

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
+  OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE if Rails.env.development?
   provider :google_oauth2, Rails.application.secrets.omniauth_provider_key, Rails.application.secrets.omniauth_provider_secret
 end
