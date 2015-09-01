d7af6e191a2599b6b92179efc578b223baa58c1b
Mihai Banulescu <mihai.banulescu@gmail.com>
2015-09-01 18:21:06 +0300

    fix some stuff

--- app/assets/javascripts/aktions.js
+++ app/assets/javascripts/aktions.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
     $('#aktions-table').DataTable({
-        order: [0, 'desc']
+        autoWidth: false,
+        ordering: false,
+        searching: false,
+        order: [0, 'desc'],
+        pageLength: 25,
+        lengthMenu: [ [10, 25, 50, -1], [10, 25, 50, 'All'] ],
+        language: { lengthMenu: "Display _MENU_ Actions" }
     });
 });
