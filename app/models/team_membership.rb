9a92970fef55fe339a49200f05f7d9e273aa3b7d
Mihai Banulescu <mihai.banulescu@gmail.com>
2015-09-01 12:24:28 +0300

    fix tm#show again

--- app/models/team_membership.rb
+++ app/models/team_membership.rb
@@ -4,6 +4,10 @@ class TeamMembership < ActiveRecord::Base
   
   validates_uniqueness_of :player_id, :scope => [:team_id], :message => "This membership is already active."
 
+  def roles
+    player.active_roles(team)
+  end
+
   def get_roles_from_api
     response = HTTParty.get(team.roles_api_url(api_key, external_id))
   end
