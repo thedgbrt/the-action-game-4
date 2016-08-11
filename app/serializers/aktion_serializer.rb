097170064c7693fbf8d52d64dbfd47e002ca2c08
Mihai <mihai.banulescu@gmail.com>
2016-08-11 06:52:09 -0700

    update aktion serialized fields

--- app/serializers/aktion_serializer.rb
+++ app/serializers/aktion_serializer.rb
@@ -1,15 +1,23 @@
 class AktionSerializer < ActiveModel::Serializer
-  attributes :id, :focus, :verb, :project_name
+  attributes :timeslot, :focus, :player_name, :verb_name, :project_name, :team_name, :role_name, :flow, :flow_notes, :value, :value_notes
   
-  def fake_rating
-    rand(30..95)
-  end
-
   def verb_name
     object.verb.try(:name)
   end
 
   def project_name
     object.project.try(:name)
+  end
+
+  def team_name
+    object.team.try(:name)
+  end
+
+  def role_name
+    object.role.try(:name)
+  end
+
+  def player_name
+    object.player.try(:name)
   end
 end
